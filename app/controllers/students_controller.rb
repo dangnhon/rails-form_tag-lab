184c4270f07397eb079927467896e485556eec74
dangnhon <dang.nhon12345@yahoo.com>
2020-12-14 10:42:45 -0800

    finally got it to work, weird mismatch instructions

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,4 +8,21 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  def new 
+    @student = Student.new
+    
+  end 
+
+  def create 
+    @new_student = Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+    # can redirect with 2 differents way: 
+
+    #1. redirect_to student_path(@new_student) 
+    #
+    # The above one basically returns you to the show page erb 
+    #
+    #2. or do it smartly like below
+    redirect_to @new_student
+  end 
+
 end
